f7c19e21fb1a95326456a3cbfa2078447183b5da
tomma90 <38280335+tomma90@users.noreply.github.com>
2020-06-25 10:42:50 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'tessimdc',
+    version = '0.0.1',
+    author = 'Tommaso Ghigna',
+    author_email = 't.ghigna@gmail.com',
+    description = 'Simulation tool for DC biased TES detectors',
+    license = 'GPLv3',
+    keywords = 'TES detectors CMB cosmology',
+    url = 'https://github.com/tomma90/TES_sim_DC',
+    packages = find_packages(),
+    include_package_data=True,
+    long_description = read('README.rst'),
+    test_suite = 'tessimdc.test'
+)
