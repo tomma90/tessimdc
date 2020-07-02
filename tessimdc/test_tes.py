5d02272adf6e2336d27e6c893120dbd24ecf23b5
tomma90 <38280335+tomma90@users.noreply.github.com>
2020-07-02 09:59:29 +0900

    Update test_tes.py

--- tessimdc/test_tes.py
+++ tessimdc/test_tes.py
@@ -2,19 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tessimdc as tes
 
-# Initiate TES
+# Initiate TES - This create an instance of a DC-biased TES with 
+# some default values to bias the TES at 0.5xRn
 tes_1 = tes.DcBiasedTesParameters()
+# You can see the default values by typing (example):
+# tes_1.bias_current
+# Default values can be modified. Example to modify bias current:
+# tes_1.modify_bias_current = 35.e-6
 
 # Define time length [s] and sampling rate [Hz]
 time = 10.
 sampling_hz = 10000
 step = int(time * sampling_hz + 1)
 
-# Basic Input with 1% sinusoidal power fluctuation
+# Basic Constant Input
 t = np.linspace(0., time, step)
 Ib = np.ones_like(t)*tes_1.bias_current
-P = np.ones_like(t)*tes_1.optical_loading_power + 0.01*tes_1.optical_loading_power*np.sin(2.*np.pi*0.25*t)
+P = np.ones_like(t)*tes_1.optical_loading_power
 Tb = np.ones_like(t)*tes_1.temperature_focal_plane
+
+# Basic Input with 1% sinusoidal power fluctuation
+# t = np.linspace(0., time, step)
+# Ib = np.ones_like(t)*tes_1.bias_current
+# P = np.ones_like(t)*tes_1.optical_loading_power + 0.01*tes_1.optical_loading_power*np.sin(2.*np.pi*0.25*t)
+# Tb = np.ones_like(t)*tes_1.temperature_focal_plane
 
 # Solve
 I, T = tes.TesRungeKuttaSolver(t, Ib, P, Tb)
